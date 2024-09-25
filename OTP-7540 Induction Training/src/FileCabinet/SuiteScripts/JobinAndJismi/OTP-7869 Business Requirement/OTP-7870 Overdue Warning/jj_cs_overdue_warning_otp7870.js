ff5c7e97eae08b09c2ac8b13cee39abf3a7b4c0e
josephantonyjobinandjismi <josephkzm1@gmail.com>
2024-09-25 17:11:23 +0530

    first commit

---
+++ OTP-7540 Induction Training/src/FileCabinet/SuiteScripts/JobinAndJismi/OTP-7869 Business Requirement/OTP-7870 Overdue Warning/jj_cs_overdue_warning_otp7870.js
@@ -0,0 +1,126 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+/**************************************************
+ * Induction Training
+ * OTP-7870 : Overdue Warning
+ * 
+ * *******************************************************************
+ * 
+ * Author : Jobin and Jismi IT Services LLP.
+ * 
+ * Date Created : 24 September 2024
+ * 
+ * Description : Give a warning to the (Sales rep) while they create a sales order for the overdue customer.
+ * REVISION HISTORY 
+ * @version  1.0 : : 24 September 2024 : Created the intial build  by  JJ0365 
+ * 
+ * 
+ * 
+ **********************************************************************************************/
+define(['N/record'],
+    /**
+     * @param{record} record
+     */
+    function (record) {
+
+        let overdueCustomer = false;
+
+        /**
+         * Function to be executed when field is changed.
+         *
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.currentRecord - Current form record
+         * @param {string} scriptContext.sublistId - Sublist name
+         * @param {string} scriptContext.fieldId - Field name
+         * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+         * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+         *
+         * @since 2015.2
+         */
+
+        /**
+         *Function to be executed when customer field is changed
+         *
+         * @param {Object} scriptContext
+         */
+        function checkCustomerOverdue(scriptContext) {
+            // Load the customer record
+            let customer = record.load({
+                type: record.Type.CUSTOMER,
+                id: scriptContext.currentRecord.getValue({
+                    fieldId: 'entity'
+                }),
+                isDynamic: true
+            })
+
+            //Get no of overdue days
+            let daysOverdue = Number(customer.getValue({
+                fieldId: 'daysoverdue'
+            }));
+
+            //Check whether the overdue days are greater than zero
+            if (daysOverdue > 0) {
+                overdueCustomer = true;
+            }
+        }
+
+        function fieldChanged(scriptContext) {
+            try {
+                const currentField = scriptContext.fieldId;
+
+                //Executed when customer field is changed
+                if (currentField === 'entity') {
+                    checkCustomerOverdue(scriptContext);
+                }
+
+            } catch (e) {
+                console.log("Error: ", e);
+            }
+        }
+
+        /**
+         * Validation function to be executed when record is saved.
+         *
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.currentRecord - Current form record
+         * @returns {boolean} Return true if record is valid
+         *
+         * @since 2015.2
+         */
+
+        function saveRecord(scriptContext) {
+            try {
+
+                if (overdueCustomer) {
+                    // Display Confirm Box
+                    if (window.confirm("You are creating sales order for an overdue customer!")) {
+                        return true;
+                    }
+                    else return false;
+                }
+
+                return true;
+
+            }
+            catch (e) {
+                console.log("Error: ", e);
+            }
+        }
+
+        return {
+            // pageInit: pageInit,
+            fieldChanged: fieldChanged,
+            // postSourcing: postSourcing,
+            // sublistChanged: sublistChanged,
+            // lineInit: lineInit,
+            // validateField: validateField,
+            // validateLine: validateLine,
+            // validateInsert: validateInsert,
+            // validateDelete: validateDelete,
+            saveRecord: saveRecord
+        };
+
+    });
