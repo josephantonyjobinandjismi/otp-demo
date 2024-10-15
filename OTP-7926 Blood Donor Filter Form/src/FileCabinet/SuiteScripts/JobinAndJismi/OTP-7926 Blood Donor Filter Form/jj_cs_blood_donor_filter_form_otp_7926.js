986598ad468373adc1731e4728c250a739a35135
josephantonyjobinandjismi <josephkzm1@gmail.com>
2024-10-15 12:06:08 +0530

    otp7926-feat-blood-donor-filter-form

---
+++ OTP-7926 Blood Donor Filter Form/src/FileCabinet/SuiteScripts/JobinAndJismi/OTP-7926 Blood Donor Filter Form/jj_cs_blood_donor_filter_form_otp_7926.js
@@ -0,0 +1,178 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+/**************************************************
+ * Induction Training
+ * OTP-7926 : Search through the database to find the matching blood donors
+ * 
+ * *******************************************************************
+ * 
+ * Author : Jobin and Jismi IT Services LLP.
+ * 
+ * Date Created : 14 October 2024
+ * 
+ * Description : Create a new form to search for the eligible blood donors, based on the filters blood group and last donation date (should be three months before) with the entered details.
+ * It must capture the details of the required blood group (and the last donation date) to find the best and eligible donors.
+ * Every eligible donor must be displayed in the form with their details (such as Name, Phone Number).
+
+ * REVISION HISTORY 
+ * @version  1.0 : : 14 October 2024 : Created the intial build  by  JJ0365 
+ * 
+ * 
+ * 
+ **********************************************************************************************/
+define(['N/record', 'N/search', 'N/format'],
+    /**
+     * @param{record} record
+     * @param{search} search
+     */
+    function (record, search, format) {
+        /**
+         * Function to be executed when field is changed.
+         *
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.currentRecord - Current form record
+         * @param {string} scriptContext.sublistId - Sublist name
+         * @param {string} scriptContext.fieldId - Field name
+         * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+         * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+         *
+         * @since 2015.2
+         */
+        function fieldChanged(scriptContext) {
+            try {
+                var currentRecord = scriptContext.currentRecord;
+                var fieldId = scriptContext.fieldId;
+    
+                // Trigger the search when the blood group or the date filter changes
+                if (fieldId === 'custpage_jj_bloodgroup' || fieldId === 'custpage_jj_lastdonationdate') {
+                    var bloodGroup = currentRecord.getValue('custpage_jj_bloodgroup');
+                    var selectedDate = currentRecord.getValue('custpage_jj_lastdonationdate'); // Selected custom date filter
+                    var filters = [];
+    
+                    if (selectedDate && bloodGroup) {
+                        var filterDate = new Date(selectedDate);
+    
+                        // Format the date for NetSuite compatibility
+                        var formattedFilterDate = format.format({
+                            value: filterDate,
+                            type: format.Type.DATE
+                        });
+    
+                        // Search for records where the last donation date is on or before the selected date
+                        filters.push(['custrecord_jj_blood_group', 'anyof', bloodGroup]);
+                        filters.push('AND');
+                        filters.push(['custrecord_jj_last_donation_date', 'onorbefore', formattedFilterDate]);   
+                    }
+
+                    else {
+                        if (bloodGroup) {
+                        // If no date is selected, apply the 3-months-before logic with blood group filter
+                        var currentDate = new Date();
+                        var threeMonthsAgo = new Date();
+                        threeMonthsAgo.setMonth(currentDate.getMonth() - 3);
+    
+                        var formattedThreeMonthsAgo = format.format({
+                            value: threeMonthsAgo,
+                            type: format.Type.DATE
+                        });
+                        
+                        // Apply the blood group filter and the 3-months-before filter
+                        filters.push(
+                            ['custrecord_jj_blood_group', 'anyof', bloodGroup],
+                            'AND',
+                            ['custrecord_jj_last_donation_date', 'onorbefore', formattedThreeMonthsAgo]
+                        );
+                    }
+
+                    else {
+                        var filterDate = new Date(selectedDate);
+    
+                        // Format the date for NetSuite compatibility
+                        var formattedFilterDate = format.format({
+                            value: filterDate,
+                            type: format.Type.DATE
+                        });
+
+                        filters.push(['custrecord_jj_last_donation_date', 'onorbefore', formattedFilterDate]);
+                    }
+                    }
+                    
+                    // Create a search to find records based on the filters
+                    var mySearch = search.create({
+                        title: "Blood Donor",
+                        type: 'customrecord_jj_blood_donor_otp7925',
+                        filters: filters,
+                        columns: [
+                            'custrecord_jj_first_name',
+                            'custrecord_jj_last_name',
+                            'custrecord_jj_phone_number',
+                            'custrecord_jj_last_donation_date'
+                        ]
+                    });
+    
+                    // Clear any existing lines in the sublist before adding new ones
+                    let sublistId = 'custpage_jj_blood_sublist';
+                    var lineCount = currentRecord.getLineCount({ sublistId: sublistId });
+                    for (var j = 0; j < lineCount; j++) {
+                        currentRecord.removeLine({
+                            sublistId: sublistId,
+                            line: 0, // Always remove the first line since lines will shift
+                            ignoreRecalc: true
+                        });
+                    }
+                    
+                    // Loop through the search results and populate the sublist
+                    var searchResult = mySearch.run();
+                    
+                    searchResult.each(function(result) {
+                        var donorFirstName = result.getValue('custrecord_jj_first_name');
+                        var donorLastName = result.getValue('custrecord_jj_last_name');
+                        var donorNumber = result.getValue('custrecord_jj_phone_number');
+        
+                        // Select a new line in the sublist
+                        currentRecord.selectNewLine({
+                            sublistId: sublistId
+                        });
+    
+                        // Set donor name and phone number in the sublist fields
+                        currentRecord.setCurrentSublistValue({
+                            sublistId: sublistId,
+                            fieldId: 'custpage_jj_first_name',
+                            value: donorFirstName
+                        });
+    
+                        currentRecord.setCurrentSublistValue({
+                            sublistId: sublistId,
+                            fieldId: 'custpage_jj_last_name',
+                            value: donorLastName
+                        });
+    
+                        currentRecord.setCurrentSublistValue({
+                            sublistId: sublistId,
+                            fieldId: 'custpage_jj_phone_number',
+                            value: donorNumber
+                        });
+
+                        // Commit the line
+                        currentRecord.commitLine({
+                            sublistId: sublistId
+                        });
+
+                        return true;
+                    });
+                }
+            }
+
+            catch (e) {
+                console.log("Error: ", e);
+            }
+        }
+
+        return {
+            fieldChanged: fieldChanged
+        };
+
+    });
---
+++ OTP-7926 Blood Donor Filter Form/src/FileCabinet/SuiteScripts/JobinAndJismi/OTP-7926 Blood Donor Filter Form/jj_sl_blood_donor_filter_form_otp_7926.js
@@ -0,0 +1,128 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+/**************************************************
+ * Induction Training
+ * OTP-7926 : Search through the database to find the matching blood donors
+ * 
+ * *******************************************************************
+ * 
+ * Author : Jobin and Jismi IT Services LLP.
+ * 
+ * Date Created : 14 October 2024
+ * 
+ * Description : Create a new form to search for the eligible blood donors, based on the filters blood group and last donation date (should be three months before) with the entered details.
+ * It must capture the details of the required blood group (and the last donation date) to find the best and eligible donors.
+ * Every eligible donor must be displayed in the form with their details (such as Name, Phone Number).
+
+ * REVISION HISTORY 
+ * @version  1.0 : : 14 October 2024 : Created the intial build  by  JJ0365 
+ * 
+ * 
+ * 
+ **********************************************************************************************/
+define(['N/format', 'N/record', 'N/search', 'N/ui/serverWidget'],
+    /**
+ * @param{format} format
+ * @param{record} record
+ * @param{search} search
+ * @param{serverWidget} serverWidget
+ */
+    (format, record, search, serverWidget) => {
+
+        /**
+         *Function to create blood donor search form
+         *
+         * @param {*} scriptContext
+         */
+        function createBloodDonorFilterForm(scriptContext) {
+            try {
+                // Create the form
+                var form = serverWidget.createForm({
+                    title: 'Search Eligible Blood Donors'
+                });
+
+                // Blood Group Filter (Dropdown)
+                var bloodGroupField = form.addField({
+                    id: 'custpage_jj_bloodgroup',
+                    type: serverWidget.FieldType.SELECT,
+                    label: 'Blood Group'
+                });
+
+                // Adding blood group options
+                bloodGroupField.addSelectOption({ value: '', text: '' });
+                bloodGroupField.addSelectOption({ value: '1', text: 'A+' });
+                bloodGroupField.addSelectOption({ value: '2', text: 'A-' });
+                bloodGroupField.addSelectOption({ value: '3', text: 'B+' });
+                bloodGroupField.addSelectOption({ value: '4', text: 'B-' });
+                bloodGroupField.addSelectOption({ value: '5', text: 'AB+' });
+                bloodGroupField.addSelectOption({ value: '6', text: 'AB-' });
+                bloodGroupField.addSelectOption({ value: '7', text: 'O+' });
+                bloodGroupField.addSelectOption({ value: '8', text: 'O-' });
+
+                // Last Donation Date Filter (Date)
+                var lastDonationDateField = form.addField({
+                    id: 'custpage_jj_lastdonationdate',
+                    type: serverWidget.FieldType.DATE,
+                    label: 'Last Donation Date'
+                });
+
+                let bloodDonarsublist = form.addSublist({
+                    id: 'custpage_jj_blood_sublist',
+                    type: serverWidget.SublistType.INLINEEDITOR,
+                    label: 'Blood Donar Details'
+                });
+
+                bloodDonarsublist.addField({
+                    id: 'custpage_jj_first_name',
+                    type: serverWidget.FieldType.TEXT,
+                    label: 'First Name'
+                });
+
+                bloodDonarsublist.addField({
+                    id: 'custpage_jj_last_name',
+                    type: serverWidget.FieldType.TEXT,
+                    label: 'Last Name'
+                });
+
+                bloodDonarsublist.addField({
+                    id: 'custpage_jj_phone_number',
+                    type: serverWidget.FieldType.PHONE,
+                    label: 'Phone Number'
+                });
+
+                form.clientScriptModulePath = "SuiteScripts/JobinAndJismi/OTP-7926 Blood Donor Filter Form/jj_cs_blood_donor_filter_form_otp_7926.js"
+
+                // Display the form to the user
+                scriptContext.response.writePage(form);
+            }
+
+            catch (e) {
+                log.error("Error: ", e);
+            }
+        }
+
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+        const onRequest = (scriptContext) => {
+            try {
+                if (scriptContext.request.method === 'GET') {
+                    // Create the form
+                    createBloodDonorFilterForm(scriptContext);
+                }
+            }
+
+            catch (e) {
+                log.error("Error: ", e);
+            }
+        }
+
+        return { onRequest }
+
+    });
